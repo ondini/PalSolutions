d28638db35c4c77fae51b666bef53734194e2ee9
Ondin <andreas.jackdaw@gmail.com>
2021-10-14 18:50:44 +0200

    02Ex, final solution containig Tarjan's algorithm for SCC, graph condensation and Dijkstra's algorithm for search for minimal paths from central point to delivery points

--- 02Ex/main.cpp
+++ 02Ex/main.cpp
@@ -2,7 +2,6 @@
 #include <cmath>
 #include <stack>
 
-#include "matrix.hpp"
 #include <bits/stdc++.h>
 
 #define UNVISITED -2
@@ -12,57 +11,45 @@ using namespace std;
 
 // Utilized approach: DFS from each hub
 
-void readConvs(vector<int> roadAdjList[], vector<int> convAdjListRev[], int numRoads);
+void readConvs(vector<vector<int>> *convAdjList, int numRoads);
+
+class CompareNodes
+{
+public:
+    bool operator()(pair<int, int> a, pair<int, int> b)
+    {
+        return a.second > b.second;
+    }
+};
+
+void printQueue(priority_queue<pair<int, int>, vector<pair<int, int>>, CompareNodes> gq);
 
 void printGraph(vector<int> adj[], int V);
 
 void printVec(vector<int> vec);
 
-void tarjanStep(int point, stack<int> *dfs_stack, vector<int> *points, int *openTime, int *lastSCC, vector<int> convAdjList[]);
+void tarjanStep(int point, stack<int> *dfs_stack, vector<int> *points, int *openTime, int *lastSCC, vector<vector<int>> *convAdjList);
 
 void printMatrix(vector<int> *matrix, int matRows, int matCols);
 
-void condGraph(int numPoints, vector<int> convAdjList[], vector<int> convAdjListRev[], vector<int> convAdjListCond[], vector<int> *points)
-{
+void condGraph(int numPoints, vector<vector<int>> *convAdjList, vector<vector<int>> *convAdjListCond, vector<vector<int>> *convAdjListCondRev, vector<int> *points);
 
-    for (int i = 0; i < numPoints; i++)
-    {
-        // if (convAdjList[i].empty())
-        // {
-        //     startPoints
-        // }
-        int succCond, compCond;
-        for (int succ : convAdjList[i])
-        {
-            compCond = points->at(i * NUMCOLS + 3);
-            succCond = points->at(succ * NUMCOLS + 3);
-            if (succCond != compCond)
-                convAdjListCond[compCond].push_back(succCond);
-            // if (std::find(convAdjListCond[points->at(i * NUMCOLS + 3)].begin(), convAdjListCond[points->at(i * NUMCOLS + 3)].end(), points->at(succ * NUMCOLS + 3)) != convAdjListCond[points->at(i * NUMCOLS + 3)].end())
-            // {
-            //     convAdjListCond[points->at(i * NUMCOLS + 3)].push_back(points->at(succ * NUMCOLS + 3));
-            // }
-        }
-    }
-}
+void SPSearch(int centrPCond, vector<int> *visited, vector<vector<int>> *convAdjListCond, vector<vector<int>> *convAdjListCondRev, priority_queue<pair<int, int>, vector<pair<int, int>>, CompareNodes> *searchPQueue);
 
 int main(int argc, char const *argv[])
 {
-
     // read metadata
     int numPoints, numConvs, iCentral;
     cin >> numPoints >> numConvs >> iCentral;
 
-    cout << numPoints << " " << numConvs << " " << iCentral << endl;
+    // variables for original graph and reversed directions graph storing
+    vector<vector<int>> convAdjList(numPoints);
+    vector<vector<int>> convAdjListRev(numPoints);
 
-    //variables for graph storing
-    vector<int> convAdjList[numPoints];
-    vector<int> convAdjListRev[numPoints];
+    // read graph
+    readConvs(&convAdjList, numConvs);
 
-    readConvs(convAdjList, convAdjListRev, numConvs);
-    printGraph(convAdjList, numPoints);
-
-    // Find SSUs using Tarjan
+    // Find SCCs using Tarjan's algorithm
 
     stack<int> dfs_stack;
     vector<int> points(numPoints * NUMCOLS, UNVISITED);
@@ -73,67 +60,73 @@ int main(int argc, char const *argv[])
     {
         if (points[point * NUMCOLS] == UNVISITED)
         {
-            //cout << "Visiting point no. " << point << endl;
-            tarjanStep(point, &dfs_stack, &points, &openTime, &lastSCC, convAdjList);
+            tarjanStep(point, &dfs_stack, &points, &openTime, &lastSCC, &convAdjList);
         }
     }
 
-    printMatrix(&points, 17, NUMCOLS);
+    // create condensed graph
+    vector<vector<int>> convAdjListCond(lastSCC);
+    vector<vector<int>> convAdjListCondRev(lastSCC);
+    condGraph(numPoints, &convAdjList, &convAdjListCond, &convAdjListCondRev, &points);
 
-    vector<int> convAdjListCond[lastSCC];
-    condGraph(numPoints, convAdjList, convAdjListRev, convAdjListCond, &points);
-    printGraph(convAdjListCond, lastSCC);
+    // search condensed graph for shortest paths from central point
+    int centrPCond = points[iCentral * NUMCOLS + 3];
+    vector<int> visited(lastSCC, -1);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, CompareNodes> searchPQueue;
+
+    SPSearch(centrPCond, &visited, &convAdjListCond, &convAdjListCondRev, &searchPQueue);
+
+    // get sum of paths to delivery points
+    int finCount = 0;
+    for (int point = 0; point < lastSCC; point++)
+    {
+        if (convAdjListCondRev[point].empty())
+            finCount += visited[point];
+    }
+
+    //printVec(visited);
+    cout << finCount << endl;
 
     return 0;
 }
 
-void tarjanStep(int point, stack<int> *dfs_stack, vector<int> *points, int *openTime, int *lastSCC, vector<int> convAdjList[])
+void tarjanStep(int point, stack<int> *dfs_stack, vector<int> *points, int *openTime, int *lastSCC, vector<vector<int>> *convAdjList)
 {
-
     points->at(point * NUMCOLS) = *openTime;
     points->at(point * NUMCOLS + 1) = *openTime;
     *openTime += 1;
     dfs_stack->push(point);
     points->at(point * NUMCOLS + 2) = 1;
 
-    vector<int> successors = convAdjList[point]; // point successors
+    vector<int> successors = convAdjList->at(point); // point successors
 
-    // cout << "Successors: ";
-    // printVec(successors);
-    // printMatrix(points, 17, NUMCOLS);
-
-    // consider each point successor
+    // consider each point's successor
     for (int successor : successors)
     {
         if (points->at(successor * NUMCOLS) == UNVISITED)
         {
-            // Successor has not yet been visited; recurse on it
-            //cout << "Not visited: " << successor << endl;
+            // successor not yet visited
             tarjanStep(successor, dfs_stack, points, openTime, lastSCC, convAdjList);
             points->at(point * NUMCOLS + 1) = min(points->at(point * NUMCOLS + 1), points->at(successor * NUMCOLS + 1));
         }
         else if (points->at(successor * NUMCOLS + 2) == 1)
         {
-            //cout << "Visited and onstack: " << successor << endl;
-            // Successor w is in stack S and hence in the current SCC
-            // If w is not on stack, then (v, w) is an edge pointing to an SCC already found and must be ignored
-            // Note: The next line may look odd - but is correct.
-            // It says w.index not w.lowlink; that is deliberate and from the original paper
+            //successor visited and is on stack
             points->at(point * NUMCOLS + 1) = min(points->at(point * NUMCOLS + 1), points->at(successor * NUMCOLS));
         }
     }
 
-    // If v is a root node, pop the stack and generate an SCC
+    // point is a root node, pop it from stack to SCC
     if (points->at(point * NUMCOLS + 1) == points->at(point * NUMCOLS))
     {
-        // start new SCC
+        // start a new SCC
         vector<int> SCC;
         int u = -1;
 
+        //go through whole new SCC
         while (u != point)
         {
             u = dfs_stack->top();
-            //cout << "Popping: " << u << endl;
             dfs_stack->pop();
             points->at(u * NUMCOLS + 2) = UNVISITED;
 
@@ -142,17 +135,64 @@ void tarjanStep(int point, stack<int> *dfs_stack, vector<int> *points, int *open
         }
 
         *lastSCC += 1;
-        // cout << "SCC " << *lastSCC << ": ";
-        // printVec(SCC);
     }
 }
 
-// void condenseGraph()
-// {
-// }
-// <<<<<<<<<<<<<<, ,>>>>>>>>>>>>>>>>>>>>>
+void condGraph(int numPoints, vector<vector<int>> *convAdjList, vector<vector<int>> *convAdjListCond, vector<vector<int>> *convAdjListCondRev, vector<int> *points)
+{
+    int succCond, compCond;
+    for (int i = 0; i < numPoints; i++)
+    {
 
-void readConvs(vector<int> convAdjList[], vector<int> convAdjListRev[], int numConvs)
+        compCond = points->at(i * NUMCOLS + 3);
+        for (int succ : convAdjList->at(i))
+        {
+            succCond = points->at(succ * NUMCOLS + 3);
+            if (succCond != compCond)
+            {
+                convAdjListCond->at(compCond).push_back(succCond);
+                convAdjListCondRev->at(succCond).push_back(compCond);
+            }
+        }
+    }
+}
+
+void SPSearch(int centrPCond, vector<int> *visited, vector<vector<int>> *convAdjListCond, vector<vector<int>> *convAdjListCondRev, priority_queue<pair<int, int>, vector<pair<int, int>>, CompareNodes> *searchPQueue)
+{
+    searchPQueue->push(make_pair(centrPCond, 0));
+    visited->at(centrPCond) = 0;
+
+    while (!searchPQueue->empty())
+    {
+        auto topElement = searchPQueue->top();
+        int point = topElement.first;
+        int price = topElement.second;
+        searchPQueue->pop();
+
+        if (visited->at(point) != -1 && visited->at(point) < price)
+            continue;
+
+        visited->at(point) = price;
+
+        for (int succ : convAdjListCond->at(point))
+        {
+            if (visited->at(succ) == -1 || visited->at(succ) > price)
+            {
+                searchPQueue->push(make_pair(succ, price));
+            }
+        }
+
+        for (int succ : convAdjListCondRev->at(point))
+        {
+            if (visited->at(succ) == -1 || visited->at(succ) > price)
+            {
+                searchPQueue->push(make_pair(succ, price + 1));
+            }
+        }
+    }
+}
+
+void readConvs(vector<vector<int>> *convAdjList, int numConvs)
 {
     // read values from input, add directly create adjacecny list
     int pointA, pointB;
@@ -160,12 +200,13 @@ void readConvs(vector<int> convAdjList[], vector<int> convAdjListRev[], int numC
     for (int i = 0; i < numConvs; ++i)
     {
         cin >> pointA >> pointB;
-        convAdjList[pointA].push_back(pointB);
-        convAdjListRev[pointB].push_back(pointA);
+        convAdjList->at(pointA).push_back(pointB);
     }
 }
 
-void printGraph(vector<int> adj[], int V)
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<, ,>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+void printGraph(vector<vector<int>> adj, int V)
 {
     for (int u = 0; u < V; u++)
     {
@@ -213,4 +254,15 @@ void printMatrix(vector<int> *matrix, int matRows, int matCols)
         cout << "____";
     }
     cout << "__" << endl;
+}
+
+void printQueue(priority_queue<pair<int, int>, vector<pair<int, int>>, CompareNodes> gq)
+{
+    priority_queue<pair<int, int>, vector<pair<int, int>>, CompareNodes> g = gq;
+    while (!g.empty())
+    {
+        cout << '\t' << g.top().first << " : " << g.top().second;
+        g.pop();
+    }
+    cout << '\n';
 }
